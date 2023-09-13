a01850dd8c67058ed14a923d499f7f6585bd0f59
ttting999999 <perplex0204@gmail.com>
2023-09-13 12:08:04 +0800

    20230913 vs

---
+++ 18.4-sum.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=18 lang=python3
+#
+# [18] 4Sum
+#
+
+from typing import List
+
+# @lc code=start
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        
+# @lc code=end
+
